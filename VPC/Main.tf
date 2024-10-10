6cd85458004f0200969aa8a50ef67762c4597898
Baaza <Baazababe@gmail.com>
2024-10-10 15:32:11 -0700

    updatesubnet

--- VPC/Main.tf
+++ VPC/Main.tf
@@ -1,120 +1,121 @@
-# create vpc
+# # create vpc
 resource "aws_vpc" "vpc" {
-  cidr_block              = 
-  instance_tenancy        = 
+  cidr_block              = var.vpc_cidr
   enable_dns_hostnames    = true
 
   tags      = {
-    Name    = "${}-vpc"
+    Name    = "${var.project_name}-vpc"
   }
 }
 
 # create internet gateway and attach it to vpc
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id    = 
+#resource "aws_internet_gateway" "internet_gateway" {
+#   vpc_id    = 
 
-  tags      = {
-    Name    = "${}-igw"
-  }
+#   tags      = {
+#     Name    = "${}-igw"
+#   }
+# }
+
+# # use data source to get all avalablility zones in region
+data "aws_availability_zones" "available_zones" {
+  state = "available"
 }
-
-# use data source to get all avalablility zones in region
-data "aws_availability_zones" "available_zones" {}
 
 # create public subnet az1
 resource "aws_subnet" "public_subnet_az1" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.0.0/32"
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  #map_public_ip_on_launch = 
 
   tags      = {
-    Name    = 
+   Name    = "${var.project_name}-publicsubnet1"
   }
 }
 
-# create public subnet az2
-resource "aws_subnet" "public_subnet_az2" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
+ # create public subnet az2
+ resource "aws_subnet" "public_subnet_az2" {
+ vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.0.0/32"
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
+   #map_public_ip_on_launch = 
 
-  tags      = {
-    Name    = 
-  }
+   tags      = {
+     Name    = "${var.project_name}-publicsubnet2"
+   }
+ }
+
+ # create route table and add public route
+ resource "aws_route_table" "public_route_table" {
+   vpc_id       = aws_vpc.vpc.id
+
+   route {
+     cidr_block = 
+     gateway_id = 
+   }
+
+#   tags       = {
+#     Name     = 
+#   }
+ }
+
+# # associate public subnet az1 to "public route table"
+ resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
+   subnet_id           = 
+   route_table_id      = 
+ }
+
+ # associate public subnet az2 to "public route table"
+ resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
+   subnet_id           = 
+   route_table_id      = 
+ }
+
+# # create private app subnet az1
+ resource "aws_subnet" "private_app_subnet_az1" {
+   vpc_id                   = 
+   cidr_block               = 
+   availability_zone        = 
+#   map_public_ip_on_launch  = 
+
+   tags      = {
+     Name    = "spacestation"
+   }
 }
 
-# create route table and add public route
-resource "aws_route_table" "public_route_table" {
-  vpc_id       = 
+# # create private app subnet az2
+# resource "aws_subnet" "private_app_subnet_az2" {
+#   vpc_id                   = 
+#   cidr_block               = 
+#   availability_zone        = 
+#   map_public_ip_on_launch  = 
 
-  route {
-    cidr_block = 
-    gateway_id = 
-  }
+#   tags      = {
+#     Name    = 
+#   }
+# }
 
-  tags       = {
-    Name     = 
-  }
-}
+# # create private data subnet az1
+ resource "aws_subnet" "private_data_subnet_az1" {
+   vpc_id                   = 
+   cidr_block               = 
+   availability_zone        = 
+#   map_public_ip_on_launch  = 
 
-# associate public subnet az1 to "public route table"
-resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
-  subnet_id           = 
-  route_table_id      = 
-}
+   tags      = {"spacestation"
+     Name    = 
+   }
+ }
 
-# associate public subnet az2 to "public route table"
-resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
-  subnet_id           = 
-  route_table_id      = 
-}
+# # create private data subnet az2
+# resource "aws_subnet" "private_data_subnet_az2" {
+#   vpc_id                   = 
+#   cidr_block               = 
+#   availability_zone        = 
+#   map_public_ip_on_launch  = 
 
-# create private app subnet az1
-resource "aws_subnet" "private_app_subnet_az1" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  = 
-
-  tags      = {
-    Name    = 
-  }
-}
-
-# create private app subnet az2
-resource "aws_subnet" "private_app_subnet_az2" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  = 
-
-  tags      = {
-    Name    = 
-  }
-}
-
-# create private data subnet az1
-resource "aws_subnet" "private_data_subnet_az1" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  = 
-
-  tags      = {
-    Name    = 
-  }
-}
-
-# create private data subnet az2
-resource "aws_subnet" "private_data_subnet_az2" {
-  vpc_id                   = 
-  cidr_block               = 
-  availability_zone        = 
-  map_public_ip_on_launch  = 
-
-  tags      = {
-    Name    = 
-  }
-}
+#   tags      = {
+#     Name    = 
+#   }
+# }
--- VPC/Variables.tf
+++ VPC/Variables.tf
@@ -1,3 +1,7 @@
-variable "region" {}
-variable "project_name" {}
-variable "vpc_cidr" {}
+#variable "region" {}
+variable "project_name" {
+    default= "Cold-fries-Hot-beer"
+}
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}

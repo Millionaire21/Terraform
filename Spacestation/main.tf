47eed1ed1469257deafa8534a1096e264a217b9d
Baaza <Baazababe@gmail.com>
2024-10-24 06:53:01 -0700

    naming

--- Spacestation/main.tf
+++ Spacestation/main.tf
@@ -6,44 +6,44 @@ resource "aws_vpc" "terraform-vpc" {
 
 # AWS Subnets
 
-resource "aws_subnet" "NewSubnet1" {
+resource "aws_subnet" "SpaceStation1" {
   cidr_block = "10.0.0.0/24"
   vpc_id = aws_vpc.terraform-vpc.id
   availability_zone = "us-east-1a"
   tags = {
     Purpose ="exercise"
-    Name = "NewSubnet1"
+    Name = "SpaceStation1"
   }
 }
 
-resource "aws_subnet" "NewSubnet2" {
+resource "aws_subnet" "SpaceStation2" {
   cidr_block = "10.0.1.0/24"
   vpc_id = aws_vpc.terraform-vpc.id
   availability_zone = "us-east-1b"
   tags = {
     Purpose= "exercise"
-    Name= "NewSubnet2"
+    Name= "SpaceStation2"
   }
 }
 
-resource "aws_subnet" "NewSubnet3" {
+resource "aws_subnet" "SpaceStation3" {
   cidr_block = "10.0.2.0/24"
   vpc_id = aws_vpc.terraform-vpc.id
   availability_zone = "us-east-1c"
   tags = {
     Purpose = "exercise"
-    Name = "NewSubnet3"
+    Name = "SpaceStation3"
   }
 }
 
 # Outputs
 
-output "NewSubnet1-id" {
-  value = aws_subnet.NewSubnet1.id
+output "SpaceStation1-id" {
+  value = aws_subnet.SpaceStation1.id
 }
-output "NewSubnet2-id" {
-  value = aws_subnet.NewSubnet2.id
+output "SpaceStation2-id" {
+  value = aws_subnet.SpaceStation2.id
 }
-output "NewSubnet3-id" {
-  value = aws_subnet.NewSubnet3.id
+output "SpaceStation3-id" {
+  value = aws_subnet.SpaceStation3.id
 }
